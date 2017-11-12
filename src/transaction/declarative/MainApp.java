cc9a13f44715d771813dadf04ffaf9f7e5693b85
EastmanJian <50935@qq.com>
2017-11-12 15:35:08 +0800

    add spring transaction framework demos

--- src/transaction/declarative/MainApp.java
+++ src/transaction/declarative/MainApp.java
@@ -1,8 +1,10 @@
-package transaction.programmatic;
+package transaction.declarative;
 
 import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
 
 /**
  * This example requires the following DB environment to run.
@@ -20,23 +22,28 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  *
  * Change the config if you ride on another DBMS.
  *
- * The StudentJDBCTemplate.crete() method is managed by transaction with a programmatic approach.
+ * The StudentJDBCTemplate.crete() method is managed by transaction using AOP declarative transaction management.
  */
 public class MainApp {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("BeansTxProgrammatic.xml");
-        StudentJDBCTemplate studentJDBCTemplate = (StudentJDBCTemplate)context.getBean("studentJDBCTemplate");
+        ApplicationContext context = new ClassPathXmlApplicationContext("BeansTxDeclarative.xml");
 
-        System.out.println("------Records creation--------" );
-        studentJDBCTemplate.create("Zara", 11, 99, 2010);
-        studentJDBCTemplate.create("Nuha", 20, 97, 2010);
-        studentJDBCTemplate.create("Ayan", 25, 100, 2011);
+        StudentDAO studentJDBCTemplate = (StudentDAO) context.getBean("studentJDBCTemplate");
 
-        System.out.println("------Listing all the records--------" );
+        System.out.println("------Records creation--------");
+        try {
+            studentJDBCTemplate.create("Zara", 11, 99, 2010);
+            studentJDBCTemplate.create("Ayan", 25, 100, 2011);
+            studentJDBCTemplate.create("Nuha", 20, 97, 2010);
+        } catch (DataAccessException e) {
+            System.out.println("MainApp - Error while inserting student records.");
+        }
+
+        System.out.println("------Listing all the records--------");
         List<StudentMarks> studentMarks = studentJDBCTemplate.listStudents();
         for (StudentMarks record : studentMarks) {
-            System.out.print("ID : " + record.getId() );
-            System.out.print(", Name : " + record.getName() );
+            System.out.print("ID : " + record.getId());
+            System.out.print(", Name : " + record.getName());
             System.out.print(", Marks : " + record.getMarks());
             System.out.print(", Year : " + record.getYear());
             System.out.println(", Age : " + record.getAge());
