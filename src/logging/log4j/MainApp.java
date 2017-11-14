fc829f521d9e8efc23ee1c0bb81ded288de0f265
EastmanJian <50935@qq.com>
2017-11-14 12:41:30 +0800

    add spring logging demos

---
+++ src/logging/log4j/MainApp.java
@@ -0,0 +1,18 @@
+package logging.log4j;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.log4j.Logger;
+
+public class MainApp {
+    static Logger log = Logger.getLogger(MainApp.class.getName());
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("BeansLoggingLog4j.xml");
+        log.info("Going to create HelloWord Obj");
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.getMessage();
+
+        log.info("Exiting the program");
+    }
+}
